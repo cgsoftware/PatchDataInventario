2c8af177a13b0c867af8a5e313d45cc1a397df8e
Giu Skipper <g.dalo@cgsoftware.it>
2011-12-30 13:02:25 +0100

    Primo_Commit

---
+++ stock.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import time
+from operator import itemgetter
+from itertools import groupby
+
+from osv import fields, osv
+from tools.translate import _
+import netsvc
+import tools
+import decimal_precision as dp
+import logging
+
+class stock_inventory(osv.osv):
+    _inherit = "stock.inventory"
+    
+    def action_confirm(self, cr, uid, ids, context=None):
+        """ Confirm the inventory and writes its finished date
+        @return: True
+        """
+        #import pdb;pdb.set_trace()
+        if context is None:
+            context = {}
+        # to perform the correct inventory corrections we need analyze stock location by
+        # location, never recursively, so we use a special context
+        product_context = dict(context, compute_child=False)
+
+        location_obj = self.pool.get('stock.location')
+        for inv in self.browse(cr, uid, ids, context=context):
+            move_ids = []
+            for line in inv.inventory_line_id:
+                #import pdb;pdb.set_trace()
+                pid = line.product_id.id
+                # RIGA ORIGINALE product_context.update(uom=line.product_uom.id, date=inv.date, prodlot_id=line.prod_lot_id.id)
+                product_context.update(uom=line.product_uom.id, to_date=inv.date, prodlot_id=line.prod_lot_id.id)
+                amount = location_obj._product_get(cr, uid, line.location_id.id, [pid], product_context)[pid]
+
+                change = line.product_qty - amount
+                lot_id = line.prod_lot_id.id
+                if change:
+                    location_id = line.product_id.product_tmpl_id.property_stock_inventory.id
+                    value = {
+                        'name': 'INV:' + str(line.inventory_id.id) + ':' + line.inventory_id.name,
+                        'product_id': line.product_id.id,
+                        'product_uom': line.product_uom.id,
+                        'prodlot_id': lot_id,
+                        'date': inv.date,
+                    }
+                    if change > 0:
+                        value.update( {
+                            'product_qty': change,
+                            'location_id': location_id,
+                            'location_dest_id': line.location_id.id,
+                        })
+                    else:
+                        value.update( {
+                            'product_qty': -change,
+                            'location_id': line.location_id.id,
+                            'location_dest_id': location_id,
+                        })
+                    move_ids.append(self._inventory_line_hook(cr, uid, line, value))
+            message = _('Inventory') + " '" + inv.name + "' "+ _("is done.")
+            self.log(cr, uid, inv.id, message)
+            self.write(cr, uid, [inv.id], {'state': 'confirm', 'move_ids': [(6, 0, move_ids)]})
+        return True
+ 
+
+stock_inventory()
+
